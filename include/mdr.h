6a132ef5945f2eeb64532c92b84bd0cbe1cec2b6
Jérémie Astor <astor.jeremie@wanadoo.fr>
2020-02-28 01:04:02 +0100

    mingw: strndup

--- include/mdr.h
+++ include/mdr.h
@@ -58,14 +58,7 @@ static inline char* empty_string(void) {
 }
 FILE* mdr_open_write(const char *str);
 #ifdef __MINGW32__
-char *strndup(const char *s, size_t n) {
-  char* c = malloc(n+1);
-  if(c) {
-    strncpy(c, s, n);
-    c[n] = '\0';
-  }
-  return c;
-}
+char *strndup(const char *s, size_t n);
 #endif
 
 // mdr.c
